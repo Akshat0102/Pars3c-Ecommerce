2e780d2a34546cc672ec8c6e247849a38e276ca9
Akshat <cruise.akshat@gmail.com>
2026-05-29 18:41:39 +0530

    feat: added redux hooks and integrated modal with dispatch methods

--- src/components/product-modal/product-modal.component.tsx
+++ src/components/product-modal/product-modal.component.tsx
@@ -1,6 +1,12 @@
 import type { ProductItem } from "../../shop-data";
 import { useEffect } from "react";
 import "./product-modal.styles.scss";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import {
+  addItem,
+  increaseQuantity,
+  decreaseQuantity,
+} from "../../store/cart/cart.slice";
 
 type ProductModalProps = {
   product: ProductItem;
@@ -8,16 +14,19 @@ type ProductModalProps = {
 };
 
 const ProductModal = ({ product, onClose }: ProductModalProps) => {
-
   useEffect(() => {
     document.body.style.overflow = "hidden";
-
     return () => {
       document.body.style.overflow = "auto";
     };
   }, []);
 
   const { name, imageUrl, price } = product;
+  const dispatch = useAppDispatch();
+  const cartItem = useAppSelector((state) =>
+    state.cart.cartItems.find((item) => item.id === product.id),
+  );
+  const quantity = cartItem?.quantity || 0;
 
   return (
     <div className="modal-overlay" onClick={onClose}>
@@ -31,7 +40,40 @@ const ProductModal = ({ product, onClose }: ProductModalProps) => {
           <p>Rich artisan quality product crafted with premium ingredients.</p>
           <div className="bottom-section">
             <span className="price">${price}</span>
-            <button className="add-btn">Add To Cart</button>
+            <button
+              className="add-btn quantity-btn"
+              onClick={() => {
+                if (quantity === 0) {
+                  dispatch(addItem(product));
+                }
+              }}
+            >
+              {quantity === 0 ? (
+                "Add To Cart"
+              ) : (
+                <div className="qty-wrapper">
+                  <span
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      dispatch(decreaseQuantity(product.id));
+                    }}
+                  >
+                    −
+                  </span>
+
+                  <span className="qty">{quantity}</span>
+
+                  <span
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      dispatch(increaseQuantity(product.id));
+                    }}
+                  >
+                    +
+                  </span>
+                </div>
+              )}
+            </button>
           </div>
         </div>
 
