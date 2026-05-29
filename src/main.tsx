7f30563d6974d3497640cc3ca657c7f47b29b886
Akshat <cruise.akshat@gmail.com>
2026-05-29 18:07:38 +0530

    feat: setup redux store and cart slice

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./store/store";
+import "./index.scss";
 
-import App from './App.tsx';
-import './index.scss';
-
-ReactDOM.createRoot(
-  document.getElementById('root')!
-).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
